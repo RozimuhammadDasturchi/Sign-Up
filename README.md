736f773a8e2cfb3bdfc66c6f3139e14c69dcbb0e
RoziMuhammad Tohirov <117262180+RozimuhammadDasturchi@users.noreply.github.com>
2022-11-05 14:51:14 +0500

    Sign-Up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Sign-Up
+# sign-up
+ 
---
+++ index.js
@@ -0,0 +1,10 @@
+const div = document.querySelector('.chek-box');
+const btn = document.querySelector('.btn-sign-up');
+const oust = document.querySelector('.oust-div')
+
+function chakBox(){
+    div.classList.toggle('chakBox');
+    btn.classList.toggle('btn-sign-in');
+    oust.classList.toggle('oust-div2')
+
+}
